660f268853f0652e093528546b8a179e11c5b6ed
Daniel Kehlet <daniel_kehlet@hotmail.com>
2022-03-28 14:11:57 +0200

    Push for så hans console virker

--- TheMementoPattern/CarControlSystem.cs
+++ TheMementoPattern/CarControlSystem.cs
@@ -10,30 +10,34 @@ namespace TheMementoPattern
     {
         private float currentSpeed;
         private RegulatorState regulatorState;
+        private ISpeedRegulator regulator;
 
-        CarControlSystem()
+        CarControlSystem(ISpeedRegulator _regulator)
         {
             currentSpeed = regulatorState.getSpeed();
+            regulator = _regulator;
         }
 
         void startAutopilot()
         {
-            return regulatorState;
+            Console.WriteLine("Starting autopilot...");
+            regulator.setSpeed(currentSpeed);
         }
 
         void stopAutopilot()
         {
-
+            Console.WriteLine("Stopping autopilot...");
+            regulatorState
         }
 
         void carDetected()
         {
-
+            Console.WriteLine("CarDetected");
         }
 
         void roadClear()
         {
-
+            Console.WriteLine("Road is clear. Restoring                                                                                                                                                                                                                                                                             ")
         }
     }
 }
