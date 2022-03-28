693217263a262ba807d9985c71fd80773e677323
HansKobber <65594199+HansKobber@users.noreply.github.com>
2022-03-28 14:21:45 +0200

    downgrading to 2019

---
+++ TheMementoPattern2019/CarControlSystem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMementoPattern
+{
+    public class CarControlSystem
+    {
+        private float currentSpeed;
+        private RegulatorState regulatorState;
+        private ISpeedRegulator regulator;
+
+        CarControlSystem(ISpeedRegulator _regulator)
+        {
+            currentSpeed = regulatorState.getSpeed();
+            regulator = _regulator;
+        }
+
+        void startAutopilot()
+        {
+            Console.WriteLine("Starting autopilot...");
+            regulator.setSpeed(currentSpeed);
+        }
+
+        void stopAutopilot()
+        {
+            Console.WriteLine("Stopping autopilot...");
+            regulatorState
+        }
+
+        void carDetected()
+        {
+            Console.WriteLine("CarDetected");
+        }
+
+        void roadClear()
+        {
+            Console.WriteLine("Road is clear. Restoring                                                                                                                                                                                                                                                                             ")
+        }
+    }
+}
