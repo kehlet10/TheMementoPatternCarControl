610e709194dd538545fe3e0adf8ea999b146f745
HansKobber <65594199+HansKobber@users.noreply.github.com>
2022-03-28 14:20:20 +0200

    wip

---
+++ ConsoleApp1/ISpeedRegulator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMementoPattern
+{
+    public interface ISpeedRegulator
+    {
+        public void setSpeed(float speed);
+
+        public void stopRegulator();
+        public void increaseSpeed();
+        public void decreaseSpeed();
+        public SpeedRegulator.RegulatorState saveSpeed();
+
+        public void restoreSpeed(SpeedRegulator.RegulatorState regulatorState);
+    }
+}
---
+++ ConsoleApp1/SpeedRegulator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMementoPattern
+{
+    public class SpeedRegulator: ISpeedRegulator
+    {
+        private float speed;
+
+        public SpeedRegulator()
+        {
+            speed = new float(0.0f);
+        }
+
+        public void setSpeed(float speed)
+        {
+            Console.Writeline("");
+        }
+
+        public void stopRegulator();
+        public void increaseSpeed();
+        public void decreaseSpeed();
+        public SpeedRegulator.RegulatorState saveSpeed();
+
+        public void restoreSpeed(SpeedRegulator.RegulatorState regulatorState);
+
+
+        public class RegulatorState
+        {
+            private float speed;
+            public RegulatorState(float _speed)
+            {
+                speed = _speed;
+            }
+
+            private float getSpeed()
+            {
+                return speed;
+            }
+
+        }
+    }
+}
