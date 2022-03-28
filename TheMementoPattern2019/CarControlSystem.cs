3a477a12fa6752f2daec71ba344ed62ec1c12634
Daniel Kehlet <daniel_kehlet@hotmail.com>
2022-03-28 14:31:44 +0200

    CarControlSystem ready for test

--- TheMementoPattern2019/CarControlSystem.cs
+++ TheMementoPattern2019/CarControlSystem.cs
@@ -9,35 +9,37 @@ namespace TheMementoPattern
     public class CarControlSystem
     {
         private float currentSpeed;
-        private RegulatorState regulatorState;
+        private SpeedRegulator.RegulatorState regulatorState;
         private ISpeedRegulator regulator;
 
-        CarControlSystem(ISpeedRegulator _regulator)
+        CarControlSystem(ISpeedRegulator _regulator, float _currentSpeed)
         {
-            currentSpeed = regulatorState.getSpeed();
+            currentSpeed = _currentSpeed;
             regulator = _regulator;
         }
 
         void startAutopilot()
         {
-            Console.WriteLine("Starting autopilot...");
+            Console.WriteLine("Starting autopilot...\n");
             regulator.setSpeed(currentSpeed);
         }
 
         void stopAutopilot()
         {
-            Console.WriteLine("Stopping autopilot...");
-            regulatorState
+            Console.WriteLine("Stopping autopilot...\n");
         }
 
         void carDetected()
         {
-            Console.WriteLine("CarDetected");
+            Console.WriteLine("CarDetected...\n");
+            stopAutopilot();
+            regulator.saveSpeed();
         }
 
         void roadClear()
         {
-            Console.WriteLine("Road is clear. Restoring                                                                                                                                                                                                                                                                             ")
+            Console.WriteLine("Road is clear. Restoring speed...\n");
+            regulator.restoreSpeed(regulatorState);
         }
     }
 }
